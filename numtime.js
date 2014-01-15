5941fbf54adae02fb6baaef3c27a034ad9e2728a
Guest <guest-X17jcV@idranichnikov-pc.(none)>
2014-01-15 14:09:18 +0400

    lib refactored

--- numtime.js
+++ numtime.js
@@ -1,19 +1,7 @@
-Number.prototype.seconds = function () {
-	return this * 1000;
-};
-
-Number.prototype.minutes = function () {
- 	return this.seconds() * 60; 
-};
-
-Number.prototype.hours = function () {
-	return this.minutes() * 60;
-};
-
-Number.prototype.days = function () {
-	return this.hours() * 24;
-};
-
-Number.prototype.weeks = function () {
-	return this.days() * 7;
-};
+(function () {
+ this.seconds = function () { return this * 1000; };
+ this.minutes = function () { return this.seconds() * 60; };
+ this.hours = function () { return this.minutes() * 60; };
+ this.days = function () { return this.hours() * 24; };
+ this.weeks = function () { return this.days() * 7; };
+}).apply(Number.prototype);
